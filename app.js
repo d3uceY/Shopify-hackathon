05c3bb6fce2b1392cc5deccdafb3577b8e265a2b
d3uceY <onyekwelujesse1234@gmail.com>
2023-11-23 04:43:40 -0800

    i improved website accessibility

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const navPopup = document.querySelector('.popup-navigation')
 navPopupBtn.addEventListener('click', () => {
     navPopup.classList.toggle('popup-navigation-open')
     notificationPopup.classList.remove('notification-popup-open')
+
+    if (navPopup.classList.contains('popup-navigation-open')) {
+        navPopupBtn.setAttribute('aria-label', 'menu dropdown is open')
+    }
+    else {
+        navPopupBtn.setAttribute('aria-label', 'menu dropdown is closed')
+    }
 })
 //navigation pop-up
 
-window
 
 //notification pop-up
 const notificationPopup = document.querySelector('.notification-popup');
@@ -17,6 +23,11 @@ const notificationButton = document.querySelector('.bell-container')
 notificationButton.addEventListener('click', () => {
     notificationPopup.classList.toggle('notification-popup-open')
     navPopup.classList.remove('popup-navigation-open')
+
+    if (notificationPopup.classList.contains('notification-popup-open')) {
+        notificationButton.setAttribute('aria-label', 'notification drop down is open')
+    }
+    else notificationButton.setAttribute('aria-label', 'notification drop down is closed')
 })
 
 //notification pop-up
@@ -30,7 +41,7 @@ const cancelButton = document.getElementById('cancelButton');
 const popop = document.getElementById('popup')
 
 cancelButton.addEventListener('click', () => {
-    popop.style.display= 'none';
+    popop.style.display = 'none';
     popop.style.transition = 'all ease-in-out 250ms'
 })
 // black pop-up
@@ -46,9 +57,11 @@ accordionBtn.addEventListener('click', () => {
     if (accordionItem.style.height == "") {
         accordionItem.style.height = 0;
         accordionItem.classList.remove('mt-2')
+        accordionBtn.setAttribute('aria-label', 'the setup guide dropdown has been closed')
     } else {
         accordionItem.style.height = ""
         accordionItem.classList.add('mt-2')
+        accordionBtn.setAttribute('aria-label', 'the setup guide dropdown has been opened')
     }
 })
 // setup guide accordion js
@@ -77,11 +90,13 @@ const counterBar = document.querySelector(".counter-bar")
 let counter = document.querySelector('#counter')
 totalCheckbox.forEach(function (checkbox) {
     checkbox.addEventListener('click', () => {
-        
+
         const checked = document.querySelectorAll('input[type="checkbox"]:checked')
         counter.innerHTML = `${checked.length} / ${totalCheckbox.length} completed`
         // counter bar js
-        counterBar.style.width = (checked.length / totalCheckbox.length) * 100 + "%"
+
+        counterBar.style.width = (checked.length / totalCheckbox.length) * 100 + "%";
+        counter.setAttribute('aria-label', `${checked.length} out of ${totalCheckbox.length} checkboxes have been checked`)
 
         // counter bar js
     })
