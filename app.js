ebbfbf94a806d77c0a85564edaace0b73ff9336d
d3uceY <onyekwelujesse1234@gmail.com>
2023-11-29 07:04:15 -0800

    made some changes

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //navigation menu pop-up
 //these select the navigation button and the menu 
 const navPopupBtn = document.querySelector('.account-name-container')
+
 const navPopup = document.querySelector('.popup-navigation')
+
 const notificationPopup = document.querySelector('.notification-popup');
 
 
 navPopupBtn.addEventListener('click', () => {
+
+
     navPopup.classList.toggle('popup-navigation-open')
+
     notificationPopup.classList.remove('notification-popup-open')
 
     ItContains = navPopup.classList.contains('popup-navigation-open')
+
     //this is to select all the menu items
     const allMenuItems = navPopup.querySelectorAll('[role="menuitem"]')
 
@@ -17,30 +23,49 @@ navPopupBtn.addEventListener('click', () => {
 
     //when its open, the aria attributes change
     if (ItContains) {
+
+
         navPopupBtn.setAttribute('aria-label', 'menu dropdown is open, use tab or keyup or keydown to navigate')
+
         navPopupBtn.setAttribute('aria-expanded', 'true')
+
         allMenuItems.item(0).focus()
+
     }
     //when its closed, the opposite happens
     if (!ItContains) {
+
+
         navPopupBtn.setAttribute('aria-label', 'menu dropdown is closed')
+
         navPopupBtn.setAttribute('aria-expanded', 'false')
+
     }
 })
 //navigation menu pop-up
 //escape keyboard event on notification menu
 function handleEscapeClickEventNotification(event) {
+
+
     if (event.key === "Escape") {
+
+
         notificationPopup.classList.remove('notification-popup-open')
     }
 }
 notificationPopup.addEventListener('keyup', handleEscapeClickEventNotification)
-//escape keyboard event on notification menu
+
+
 
 //escape keyboard event on navigation menu
 function handleEscapeEvent(event) {
+
+
     if (event.key === "Escape") {
+
+
         navPopup.classList.remove('popup-navigation-open')
+
     }
 }
 
@@ -52,24 +77,35 @@ navPopup.addEventListener('keyup', handleEscapeEvent)
 // this targets all the menu items
 const allMenuItems = navPopup.querySelectorAll('[role="menuitem"]');
 
+
 // this is the initial count
 let active = -1;
 
 navPopup.addEventListener('keydown', (e) => {
 
+
     if (e.keyCode == 40) {
 
         if (active < allMenuItems.length - 1) {
+
+
             active++
+
             //this focuses on the item on the active
             allMenuItems[active].focus()
+
         }
 
     }
     else if (e.keyCode == 38) {
 
+
+
         if (active > -1) {
+
+
             active--
+
             //this focuses on the item on the active
             allMenuItems[active].focus()
         }
@@ -84,6 +120,8 @@ navPopup.addEventListener('keydown', (e) => {
 const notificationButton = document.querySelector('.bell-container')
 
 notificationButton.addEventListener('click', () => {
+
+
     //this toggles the popup on the notification
     notificationPopup.classList.toggle('notification-popup-open')
 
@@ -94,13 +132,17 @@ notificationButton.addEventListener('click', () => {
     const allMenuItems = notificationPopup.querySelectorAll('[role="menuitem"]')
 
     if (notificationPopup.classList.contains('notification-popup-open')) {
+
+
         notificationButton.setAttribute('aria-label', 'notification drop down is open')
+
         notificationPopup.setAttribute('aria-expanded', 'true')
 
         //if the notification popup is expanded it focuses on the first menu item in the menu
         allMenuItems.item(0).focus()
     }
     else notificationButton.setAttribute('aria-label', 'notification drop down is closed')
+
     notificationPopup.setAttribute('aria-expanded', 'false')
 })
 
@@ -112,11 +154,16 @@ notificationButton.addEventListener('click', () => {
 
 // black pop-up
 const cancelButton = document.getElementById('cancelButton');
+
 const popop = document.getElementById('popup')
 
 cancelButton.addEventListener('click', () => {
+
+
     popop.style.display = 'none';
+
     popop.style.transition = 'all ease-in-out 250ms'
+
 })
 // black pop-up
 
@@ -128,6 +175,7 @@ const accordionBtn = document.getElementById('accordionBtn')
 
 accordionBtn.addEventListener('click', () => {
 
+
     //this is the item being expanded
     const accordionItem = document.getElementById('accordionItem')
 
@@ -135,14 +183,25 @@ accordionBtn.addEventListener('click', () => {
     accordionBtn.classList.toggle('arrow-rotate')
 
     if (accordionItem.style.height == "") {
+
+
         accordionItem.style.height = 0;
+
         accordionItem.classList.remove('mt-2')
+
         accordionBtn.setAttribute('aria-label', 'the setup guide dropdown has been closed')
+
         accordionBtn.ariaExpanded = 'false'
+
     } else {
+
+
         accordionItem.style.height = ""
+
         accordionItem.classList.add('mt-2')
+
         accordionBtn.setAttribute('aria-label', 'the setup guide dropdown has been opened')
+
         accordionBtn.ariaExpanded = 'true'
     }
 })
@@ -156,13 +215,18 @@ const blocks = document.querySelectorAll('.block');
 //for each of the blocks being targeted
 blocks.forEach(function (block) {
 
+
     //when the block beign iterated over is clicked
     block.addEventListener('click', () => {
 
         //it collapses all the blocks
         blocks.forEach(function (d) {
+
+
             d.classList.add('block-collapse')
+
         })
+        
 
         //then it expands the block that was clicked initially
         block.classList.remove('block-collapse')
